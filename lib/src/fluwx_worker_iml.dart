ad8c267185889c7ee415a48a4e29aacacdfc9264
LF <huanglongfei@agora.io>
2023-06-30 20:12:42 +0800

    fix null safe bug

--- lib/src/fluwx_worker_iml.dart
+++ lib/src/fluwx_worker_iml.dart
@@ -1,31 +1,31 @@
-
 import 'dart:async';
-
 import 'package:flutter/services.dart';
-
 import 'response_models.dart';
 
-StreamController<WeChatWorkAuthResponse> _responseAuthController = new StreamController.broadcast();
+StreamController<WeChatWorkAuthResponse> _responseAuthController =
+    new StreamController.broadcast();
 
 /// Response from auth
-Stream<WeChatWorkAuthResponse> get responseFromAuth => _responseAuthController.stream;
+Stream<WeChatWorkAuthResponse> get responseFromAuth =>
+    _responseAuthController.stream;
 
 final MethodChannel _channel = const MethodChannel('fluwx_worker')
   ..setMethodCallHandler(_handler);
 
 Future<dynamic> _handler(MethodCall methodCall) {
   if ("onAuthResponse" == methodCall.method) {
-    _responseAuthController.add(WeChatWorkAuthResponse.fromMap(methodCall.arguments));
+    _responseAuthController
+        .add(WeChatWorkAuthResponse.fromMap(methodCall.arguments));
   }
 
   return Future.value(true);
 }
 
-Future register({String schema,String corpId,String agentId}) async {
-  return await _channel.invokeMethod('registerApp',{
-    'schema':schema,
-    'corpId':corpId,
-    'agentId':agentId,
+Future register(String schema, String corpId, String agentId) async {
+  return await _channel.invokeMethod('registerApp', {
+    'schema': schema,
+    'corpId': corpId,
+    'agentId': agentId,
   });
 }
 
@@ -33,19 +33,22 @@ Future isWeChatInstalled() async {
   return await _channel.invokeMethod("isWeChatInstalled");
 }
 
-Future sendAuth({String schema,String appId,String agentId,String state}) async {
-  if (state == null || state.length == 0){
+Future sendAuth(
+  String schema,
+  String appId,
+  String agentId,
+  String state,
+) async {
+  if (state.isEmpty) {
     state = DateTime.now().millisecondsSinceEpoch.toString();
   }
 
-  return await _channel.invokeMethod('sendAuth',{
-    'schema':schema,
-    'appId':appId,
-    'agentId':agentId,
-    'state':state
-  });
+  return await _channel.invokeMethod(
+    'sendAuth',
+    {'schema': schema, 'appId': appId, 'agentId': agentId, 'state': state},
+  );
 }
 
-void dispose(){
+void dispose() {
   _responseAuthController.close();
 }
